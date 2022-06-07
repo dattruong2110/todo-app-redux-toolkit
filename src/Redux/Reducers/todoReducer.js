bf41e2e16fcb9b1c9fffc5df449d92fe75c6fb8e
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-06-07 16:33:39 +0700

    Update todoReducer.js

--- src/Redux/Reducers/todoReducer.js
+++ src/Redux/Reducers/todoReducer.js
@@ -1,35 +1,33 @@
-export const initialState = JSON.parse(localStorage.getItem('todo_list')) || [];
+import { createSlice } from '@reduxjs/toolkit';
 
 const saveStateToStorage = (state) => {
     localStorage.setItem('todo_list', JSON.stringify(state));
 }
 
-export const todoReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'ADD_TODO': {
-            const newList = [...state];
-            newList.push(action.payload);
-
-            saveStateToStorage(newList);
-
-            return newList;
-        }
-        case 'UPDATE_TODO': {
-            const todoToUpdate = state.find(todo => todo.id === action.payload.id);
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState: JSON.parse(localStorage.getItem('todo_list')) || [],
+    reducers: {
+        todoAdd: (state, action) => {
+            state.push(action.payload);
+            saveStateToStorage(state);
+        },
+        
+        todoUpdate: (state, action) => {
+            const todoToUpdate = state.find(todo => todo.id === action.payload?.id);
             if (!todoToUpdate) {
                 return state;
             }
-
+            
             todoToUpdate.status = action.payload.status;
 
             state.splice(state.indexOf(todoToUpdate), 1, { ...todoToUpdate });
-
-            saveStateToStorage(state);
             
-            return [...state];
-        }
-        default: {
-            return state;
+            saveStateToStorage(state);
         }
     }
-}
+})
+
+export const { todoAdd, todoUpdate } = todoSlice.actions;
+
+export default todoSlice.reducer;
